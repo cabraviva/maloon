045d0e590ae0b4af194d33e7c3430ae9fc281c0f
Green_Lab <greencoder001@outlook.com>
2023-05-05 21:42:46 +0200

    Depends component should work now

--- src/Depends.svelte
+++ src/Depends.svelte
@@ -6,22 +6,24 @@
 
     if (typeof on !== 'string') throw new TypeError('on has to be of type string at <Depends>')
 
-    let element: HTMLDivElement
+    $: element = null
     
-    if (!window.__maloon__.wantsToSaveData()) {
-        if (typeof (IntersectionObserver) !== "undefined") {
-            const observer = new IntersectionObserver((entries) => {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        observer.unobserve(element)
-                        // Prefetch page
-                        window.__maloon__.lpc(on)
-                    }
+    $: if (element) {
+        if (!window.__maloon__.wantsToSaveData()) {
+            if (typeof (IntersectionObserver) !== "undefined") {
+                const observer = new IntersectionObserver((entries) => {
+                    entries.forEach(entry => {
+                        if (entry.isIntersecting) {
+                            observer.unobserve(element)
+                            // Prefetch page
+                            window.__maloon__.lpc(on)
+                        }
+                    })
                 })
-            })
-            observer.observe(element)
-        } else {
-            console.warn('Browser doesn\'t support IntersectionObserver, won\'t preload pages')
+                observer.observe(element)
+            } else {
+                console.warn('Browser doesn\'t support IntersectionObserver, won\'t preload pages')
+            }
         }
     }
 </script>
